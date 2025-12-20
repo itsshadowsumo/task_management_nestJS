09674d228f9255e0c5dd612e57d7717991eb80ed
puneet9572 <itspuneetdev@gmail.com>
2025-12-20 12:06:38 +0530

    prisma-postgres setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  getHello(): any {
+    throw new Error('Method not implemented.');
+  }
+  constructor(private readonly prisma: PrismaService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('test-db')
+  async test(){
+    return this.prisma.user.findMany();
   }
 }
